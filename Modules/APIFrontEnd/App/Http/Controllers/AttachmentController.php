9ee03d471365010b0e8aa82c80822918f7b29df7
seang kong heng <konghengseang878@gmail.com>
2026-01-28 00:55:44 +0700

    download file

--- Modules/APIFrontEnd/App/Http/Controllers/AttachmentController.php
+++ Modules/APIFrontEnd/App/Http/Controllers/AttachmentController.php
@@ -15,12 +15,27 @@ class AttachmentController extends Controller
      */
     public function index()
     {
-        $attachments = Attachment::orderBy('id','desc')->where('is_public',1)->get();
+        $attachments = Attachment::orderBy('id', 'desc')->where('is_public', 1)->get();
 
         return response()->json([
-            'attachments'=>$attachments
+            'attachments' => $attachments
         ]);
     }
+    public function download($id)
+    {
+        $attachment = Attachment::findOrFail($id);
+        $files = json_decode($attachment->file, true);
+        $file = $files[0];
+
+        $path = storage_path('app/' . $file['path']);
+
+        if (!file_exists($path)) {
+            return response()->json(['message' => 'File not found'], 404);
+        }
+
+        return response()->download($path, $file['name']);
+    }
+
 
 
 }
--- Modules/APIFrontEnd/routes/api.php
+++ Modules/APIFrontEnd/routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use Modules\APIFrontEnd\App\Http\Controllers\AttachmentController;
 use Modules\APIFrontEnd\App\Http\Controllers\MarketplaceController;
 use Modules\APIFrontEnd\App\Http\Controllers\OrderController;
 use Modules\APIFrontEnd\App\Http\Controllers\RegisterController\RegisterController;
 use Modules\APIFrontEnd\App\Http\Controllers\SubcriptionController;
+use Modules\Dashboard\App\Models\Attachment;
 
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('user/login', [RegisterController::class, 'login']);
@@ -18,10 +20,31 @@ Route::middleware('auth:sanctum')->get('/me', [RegisterController::class, 'usern
 
 Route::get('/marketplace', action: [MarketplaceController::class, 'index'])->name('index');
 Route::get('/marketplace/{uuid}', [MarketplaceController::class, 'show']);
+Route::get('attachment/download/{id}/{fileIndex}', function ($id, $fileIndex) {
+    $attachment = Attachment::findOrFail($id);
+
+    $files = json_decode($attachment->file, true);
+
+    if (!isset($files[$fileIndex])) {
+        abort(404);
+    }
+
+    $file = $files[$fileIndex];
+
+    // Increment download count
+    $attachment->total_downloads += 1;
+    $attachment->save();
+
+    $path = public_path($file['path']);
+    return Response::download($path, $file['name']);
+});
 
 
 // -------------------------------- Attachemnt-------------------------------
 Route::get('/attachment', action: [AttachmentController::class, 'index'])->name('index');
+// routes/api.php
+Route::get('/attachment/download/{id}', [AttachmentController::class, 'download']);
+
 
 // --------------------------------Order-------------------------------
 // Route::post('/order', action: [OrderController::class, 'store']);
