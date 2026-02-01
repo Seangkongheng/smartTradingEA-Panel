f9e941e1e93a16ff9630a6efc92264ae97311d58
seang kong heng <konghengseang878@gmail.com>
2026-02-01 15:03:23 +0700

    fix(): core

--- config/cors.php
+++ config/cors.php
@@ -7,15 +7,12 @@ return [
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
-        'https://smarttradingea.com/',
-        'https://vsadmin.smarttradingea.com/',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://smarttradingea.com',
+        'https://vsadmin.smarttradingea.com',
     ],
 
-
-    // 'allowed_origins' => [
-    //     'http://localhost:5173',
-    //     'http://127.0.0.1:5173',
-    // ],
 
     'allowed_origins_patterns' => [],
 
