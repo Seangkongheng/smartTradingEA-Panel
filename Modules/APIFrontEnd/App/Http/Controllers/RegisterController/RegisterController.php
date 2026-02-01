ea399502fe0b24d7ed6a340a1c0fa7de44f4be92
seang kong heng <konghengseang878@gmail.com>
2026-02-01 18:23:17 +0700

    verify with login

--- Modules/APIFrontEnd/App/Http/Controllers/RegisterController/RegisterController.php
+++ Modules/APIFrontEnd/App/Http/Controllers/RegisterController/RegisterController.php
@@ -129,16 +129,6 @@ class RegisterController extends Controller
                 ], 401);
             }
 
-
-            if ($user->is_verify) {
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Login successful',
-                    'user' => $user,
-                    'token' => $user->createToken('auth')->plainTextToken,
-                ]);
-            }
-
             // Generate login verification token
             $loginToken = Str::uuid();
 
@@ -146,7 +136,6 @@ class RegisterController extends Controller
                 'login_verify_token' => $loginToken,
                 'verification_expires_at' => Carbon::now('Asia/Phnom_Penh')->addMinutes(10),
             ]);
-
 
             $url = config('app.frontend_url')
                 . "/verify-login?token={$loginToken}&user={$user->id}";
@@ -191,7 +180,7 @@ class RegisterController extends Controller
         $user->update([
             'login_verify_token' => null,
             'verification_expires_at' => null,
-            'is_verify' => 1
+            'is_verify'=>1
         ]);
 
         // Now issue access token
